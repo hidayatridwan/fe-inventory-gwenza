df05abfa3876bbedbd0d3ead70a3f0817d54342f
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2024-06-18 21:18:51 +0700

    debug product

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,45 @@
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <div className="container text-center">
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-body d-flex flex-column gap-3"></div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <div className="card-body d-flex flex-column gap-3"></div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <div className="card-body d-flex flex-column gap-3"></div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <div className="card">
+              <div className="card-body d-flex flex-column gap-3"></div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <div className="card-body d-flex flex-column gap-3"></div>
+            </div>
+          </div>
+          <div className="col">
+            <div className="card">
+              <div className="card-body d-flex flex-column gap-3"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Dashboard;
