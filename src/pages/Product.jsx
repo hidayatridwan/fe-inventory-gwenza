cbd86e10ec43e347083701b3c1eb897e2f8d03ef
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2024-06-18 15:42:27 +0700

    debug product

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useFetchProducts, useDeleteProduct } from "../hooks/product";
+import { useFetchTailors, useDeleteTailor } from "../hooks/tailor";
 import { useRef, useState } from "react";
 
 function Product() {
@@ -9,19 +9,21 @@ function Product() {
   const searchSizeRef = useRef();
   const [filters, setFilters] = useState({ page: 1, size: 10 });
   const [activeDropdown, setActiveDropdown] = useState(null);
-  const { data, isLoading, isError, error } = useFetchProducts({});
+  const { data, isLoading, isError, error } = useFetchTailors(filters);
   const {
     mutate: deleteMutate,
     isLoading: isDeleting,
     isError: isDeleteError,
     error: deleteError,
-  } = useDeleteProduct();
+  } = useDeleteTailor();
 
-  const handleEdit = (productId) => navigate(`/products/${productId}`);
+  const handleEdit = (tailorId) => {
+    navigate(`/tailors/${tailorId}`);
+  };
 
-  const handleDelete = (productId) => {
-    if (window.confirm("Are you sure you want to delete this product?")) {
-      deleteMutate({ productId });
+  const handleDelete = (tailorId) => {
+    if (window.confirm("Are you sure you want to delete this tailor?")) {
+      deleteMutate({ tailorId });
     }
   };
 
@@ -48,50 +50,50 @@ function Product() {
     });
   };
 
-  const toggleDropdown = (productId) => {
-    setActiveDropdown((prev) => (prev === productId ? null : productId));
+  const toggleDropdown = (tailorId) => {
+    setActiveDropdown((prev) => (prev === tailorId ? null : tailorId));
   };
 
   const renderTable = () => (
-    <table className="table table-striped table-responsive">
+    <table className="table table-striped">
       <thead>
         <tr>
-          <th>Kode Produk</th>
-          <th>Nama Produk</th>
-          <th>HPP</th>
-          <th>Harga Jual</th>
-          <th>Action</th>
+          <th>No</th>
+          <th>Nama</th>
+          <th>No HP</th>
+          <th>Alamat</th>
+          <th className="text-center">Action</th>
         </tr>
       </thead>
       <tbody>
-        {data.data.map((product) => (
-          <tr key={product.product_id}>
-            <td>{product.product_code}</td>
-            <td>{product.product_name}</td>
-            <td>{product.cost_price}</td>
-            <td>{product.selling_price}</td>
+        {data.data.map((tailor, index) => (
+          <tr key={tailor.tailor_id}>
+            <td>{index + 1}</td>
+            <td>{tailor.tailor_name}</td>
+            <td>{tailor.phone_number}</td>
+            <td>{tailor.address}</td>
             <td className="text-center">
               <div className="dropdown">
                 <button
                   type="button"
-                  onClick={() => toggleDropdown(product.product_id)}
+                  onClick={() => toggleDropdown(tailor.tailor_id)}
                   className={`btn btn-sm btn-secondary dropdown-toggle ${
-                    activeDropdown === product.product_id ? "show" : ""
+                    activeDropdown === tailor.tailor_id ? "show" : ""
                   }`}
                   data-bs-toggle="dropdown"
-                  aria-expanded={activeDropdown === product.product_id}
+                  aria-expanded={activeDropdown === tailor.tailor_id}
                 >
                   Action
                 </button>
                 <ul
                   className={`dropdown-menu ${
-                    activeDropdown === product.product_id ? "show" : ""
+                    activeDropdown === tailor.tailor_id ? "show" : ""
                   }`}
                 >
                   <li>
                     <button
                       className="dropdown-item"
-                      onClick={() => handleEdit(product.product_id)}
+                      onClick={() => handleEdit(tailor.tailor_id)}
                     >
                       Edit
                     </button>
@@ -99,7 +101,7 @@ function Product() {
                   <li>
                     <button
                       className="dropdown-item"
-                      onClick={() => handleDelete(product.product_id)}
+                      onClick={() => handleDelete(tailor.tailor_id)}
                     >
                       Delete
                     </button>
@@ -114,8 +116,21 @@ function Product() {
   );
 
   const renderPagination = () => (
-    <div className="d-flex justify-content-between align-items-start">
-      <ul className="d-flex align-items-center list-unstyled gap-2">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "start",
+      }}
+    >
+      <ul
+        style={{
+          display: "flex",
+          alignItems: "center",
+          listStyle: "none",
+          gap: "0.5rem",
+        }}
+      >
         <li>Page</li>
         <li>
           <select
@@ -134,7 +149,14 @@ function Product() {
           </select>
         </li>
       </ul>
-      <ul className="d-flex align-items-center list-unstyled gap-2">
+      <ul
+        style={{
+          display: "flex",
+          alignItems: "center",
+          listStyle: "none",
+          gap: "0.5rem",
+        }}
+      >
         <li>{`${Math.min(
           filters.page * filters.size - filters.size + 1,
           data.paging.total_items
@@ -166,16 +188,20 @@ function Product() {
 
   return (
     <>
-      <h1>Produk</h1>
+      <h1>Pengrajin</h1>
       <div className="card">
         <div
-          className="card-body d-flex flex-column gap-3"
-          style={{ overflowX: "scroll" }}
+          className="card-body"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "1rem",
+            overflowX: "scroll",
+          }}
         >
           <div className="input-group">
             <select ref={searchFieldRef} className="form-control">
-              <option value="product_code">Kode Produk</option>
-              <option value="product_name">Nama Produk</option>
+              <option value="tailor_name">Nama</option>
             </select>
             <input
               ref={searchTermRef}
@@ -186,7 +212,7 @@ function Product() {
           </div>
           <button
             className="btn btn-primary"
-            onClick={() => navigate("/products/new")}
+            onClick={() => navigate("/tailors/new")}
           >
             Add new
           </button>
