f4cb587f1241f789fd6e709dd55886a54888aed0
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2025-01-19 05:55:21 +0700

    change port running

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Bind to all IP addresses
+    port: 3001, // Set the desired port
+  },
+});
